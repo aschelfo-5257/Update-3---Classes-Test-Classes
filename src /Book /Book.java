4a7ed1288b747f3b922d8a730e0e65530bcd7f6b
Aaron Schelfo <schelfoaaron918@gmail.com>
2025-07-11 12:00:11 -0400

    Book.java

--- src /Book /Book.java
+++ src /Book /Book.java
@@ -1,42 +1,31 @@
-// Book Class
 class Book {
     private String title;
-    private String author;
-    private String isbn;
     private boolean isBorrowed;
 
-    public Book(String title, String author, String isbn) {
-        this.title = title;
-        this.author = author;
-        this.isbn = isbn;
-        this.isBorrowed = false; // Initially not borrowed
-
     // Getters
+    public Book(String title) {
+        this.title = title;
+        this.isBorrowed = false;
+    }
+
     public String getTitle() {
-      return title;
+        return title;
     }
 
-    public String getAuthor() {
-      return author;
+    public boolean isBorrowed() {
+        return isBorrowed;
     }
 
-    public String getIsbn() {
-      return isbn;
+    public void borrow() {
+        this.isBorrowed = true;
     }
 
-    public String isBorrowed() {
-      return isBorrowed;
+    public void returnBook() {
+        this.isBorrowed = false;
     }
 
-    // Setters (if needed, e.g., to mark a book as borrowed)
-    public void setBorrowed(boolean borrowed) {
-        isBorrowed = borrowed;
-    }
-  
-    public void displayBookDetails() {
-        System.out.println("Title: " + title);
-        System.out.println("Author: " + author);
-        System.out.println("ISBN: " + isbn);
-        System.out.println("Status: " + (isBorrowed ? "Borrowed" : "Available"));
+    @Override
+    public String toString() {
+        return title + (isBorrowed ? " (Borrowed)" : " (Available)");
     }
 }
